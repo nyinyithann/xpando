a5379a53c882b4f5bfb45558b23a7873c5443f95
nyi nyi than <nyinyithann@gmail.com>
2021-02-08 22:12:11 +0800

    implemented finished for Version 0.0.1

--- src/uniq/uniq.union.js
+++ src/uniq/uniq.union.js
@@ -1,20 +1,20 @@
 import { throwIfNullOrUndefined } from '../throwHelper';
 import Uniq from './uniq.core';
 
-function union(...args) {
+function union(...others) {
   throwIfNullOrUndefined(this, 'this');
 
-  if (args.some((x) => !(x instanceof Set || x instanceof Uniq))) {
+  if (others.some((x) => !(x instanceof Set || x instanceof Uniq))) {
     throw new TypeError('arguments must contain instance of type Set or Uniq.');
   }
 
-  if (args.length === 0) {
+  if (others.length === 0) {
     return this;
   }
 
   const uniq = new Uniq(this);
-  for (let i = 0; i < args.length; i += 1) {
-    for (const v of args[i]) {
+  for (let i = 0; i < others.length; i += 1) {
+    for (const v of others[i]) {
       uniq.add(v);
     }
   }
--- tests/specs.uniq/uniq.intersect.spec.js
+++ tests/specs.uniq/uniq.intersect.spec.js
@@ -1,17 +1,25 @@
 import Uniq from '../../src/uniq/uniq.main';
+import Vec from '../../src/vec/vec.core';
 
 describe('intersect()', () => {
   test('should throw TypeError for invalid arguments', () => {
     const intersect = Uniq.prototype.intersect;
     expect(() => intersect.call(null, new Uniq())).toThrow(TypeError);
-    expect(() => intersect.call(new Uniq(), null)).toThrow(TypeError);
+    expect(() => intersect.call(new Uniq(), new Vec(), [], { n: 10 })).toThrow(TypeError);
+    expect(() => intersect.call(new Uniq(), { n: 10 })).toThrow(TypeError);
   });
 
   test('should return correct result', () => {
     const s1 = Uniq.of('a', 'b', 'c', 'd');
     const s2 = Uniq.of('b', 'd', 'e');
-    expect(s1.intersect(s2)).toStrictEqual(Uniq.of('b', 'd'));
+    const s3 = Uniq.of('b', 'c', 'e');
+
+    expect(s1.intersect()).toStrictEqual(Uniq.empty());
     expect(Uniq.of().intersect(Uniq.of())).toStrictEqual(Uniq.empty());
+
+    expect(s1.intersect(s2)).toStrictEqual(Uniq.of('b', 'd'));
     expect(Uniq.prototype.intersect.call(s2, s1)).toStrictEqual(Uniq.of('b', 'd'));
+
+    expect(s1.intersect(s2, s3)).toStrictEqual(Uniq.of('b'));
   });
 });
